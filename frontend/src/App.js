0a951c456275d6c0d82f70d55f2a3d45708a3fd5
mario.meza <uriel19961810@gmail.com>
2020-09-30 19:06:53 -0500

    second commit

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React, { useReducer } from 'react';
+import './App.css';
+import { AuthContext } from './context/AuthContext';
+import { authReducer, LOGIN } from './reducers/authReducer';
+import AppRouter from './routers/AppRouter';
+import axios from 'axios'
+
+const App = () => {
+
+  const [data, dispatch] = useReducer(authReducer, {token: null})
+  const loginThunk = async (username, password) => {
+    const res = await axios.post(
+      'http://localhost:7000/auth/',
+      { username: username, password: password}
+    )
+    console.log(res.data.token)
+    dispatch({ type: LOGIN, token: res.data.token })
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      data,
+      loginThunk
+    }}>
+      <AppRouter/>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/HomeComponent.js
@@ -0,0 +1,35 @@
+import React, { useContext, useState, useEffect } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import axios from 'axios'
+
+
+const HomeComponent = () => {
+
+    const { data } = useContext(AuthContext)
+    const [user, setUser] = useState(null)
+
+
+    useEffect(() => {
+        axios.get(
+            'http://localhost:7000/users/user',
+            {headers : {
+                "Authorization": `Token ${data.token}`
+            }}
+        )
+            .then((res) => {
+                setUser(res.data)
+            })
+    }, [])
+
+
+
+    return (
+        <div>
+            <h1>Home Screen</h1>
+
+        <pre>{JSON.stringify(user, null, 4)}</pre>
+        </div>
+    )
+}
+
+export default HomeComponent
